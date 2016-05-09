33f6e41f003a7ea9d493d92418d0e08d9ed56e47
vimal raj <vimalraj@vimals-macbook-pro.local>
2016-05-09 13:33:12 -0700

    Initial Commit

---
+++ PitchPerfect2Tests/PitchPerfect2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  PitchPerfect2Tests.swift
+//  PitchPerfect2Tests
+//
+//  Created by vimal raj on 5/9/16.
+//  Copyright © 2016 vimal raj. All rights reserved.
+//
+
+import XCTest
+@testable import PitchPerfect2
+
+class PitchPerfect2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
